1bdf91459483b0f04c08f607e2f5d46a595abd70
yuvraj729 <ys882764@gmail.com>
2026-09-17 15:52:08 +0530

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCN2a4x2SNhS0NdXdhAxaYfSmFMbdmliOg",
+  authDomain: "next-topper-3fcfb.firebaseapp.com",
+  projectId: "next-topper-3fcfb",
+  storageBucket: "next-topper-3fcfb.firebasestorage.app",
+  messagingSenderId: "954433279294",
+  appId: "1:954433279294:web:f6d1a339edcd4f4701086f"
+};
